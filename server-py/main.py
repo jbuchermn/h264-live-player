c4e0a80d390c51303308a2279018c9a53158687e
Jonas Bucher <j.bucher.mn@gmail.com>
2018-10-14 16:48:12 +0000

    MJPEG support

--- server-py/main.py
+++ server-py/main.py
@@ -1,12 +1,14 @@
-from rpi_h264_stream import RPiH264Stream, RPiCameraConfig
+from rpi_stream import RPiStream, RPiCameraConfig
 from ws_server import WSServer
 
 if __name__ == '__main__':
-    stream = RPiH264Stream(RPiCameraConfig(
-        width=1280,
-        height=720,
-        fps=10,
-        quality=10))
+    stream = RPiStream(RPiCameraConfig(
+        format='mjpeg',
+        bps=10000000,
+        width=640,
+        height=480,
+        fps=3,
+        quality=20))
 
     server = WSServer(5001)
     stream.on_frame(server.on_frame)
--- server-py/rpi_stream.py
+++ server-py/rpi_stream.py
@@ -1,8 +1,6 @@
 import picamera
 import time
 
-NAL_SEP = b'\x00\x00\x00\x01'
-
 
 class RPiCameraConfig:
     def __init__(self, **kwargs):
@@ -11,6 +9,7 @@ class RPiCameraConfig:
         self.fps = 30 if 'fps' not in kwargs else kwargs['fps']
         self.bps = 0 if 'bps' not in kwargs else kwargs['bps']
         self.quality = 20 if 'quality' not in kwargs else kwargs['quality']
+        self.format = 'h264' if 'format' not in kwargs else kwargs['format']
         self.profile = 'baseline' if 'profile' not in kwargs else kwargs['profile']
 
     def start_recording(self, camera, stream):
@@ -18,17 +17,22 @@ class RPiCameraConfig:
         camera.framerate = self.fps
 
         camera.start_recording(stream,
-                               format='h264',
+                               format=self.format,
                                profile=self.profile,
                                quality=self.quality,
                                bitrate=self.bps)
 
 
-class RPiH264Stream:
+class RPiStream:
     def __init__(self, rpi_camera_config):
         self._dat = bytearray()
         self._frame_listeners = []
         self._camera = rpi_camera_config
+        self._sep = None
+        if self._camera.format == 'h264':
+            self._sep = b'\x00\x00\x00\x01'
+        elif self._camera.format == 'mjpeg':
+            self._sep = b'\xFF\xD8'
 
     def on_frame(self, callback):
         self._frame_listeners += [callback]
@@ -39,12 +43,12 @@ class RPiH264Stream:
 
     def write(self, b):
         self._dat += b
-        nal = self._dat.find(NAL_SEP, len(NAL_SEP))
+        nal = self._dat.find(self._sep, len(self._sep))
         while nal != -1:
             frame = self._dat[0:nal]
             self._dat = self._dat[nal:]
             self._on_frame(frame)
-            nal = self._dat.find(NAL_SEP, nal + len(NAL_SEP))
+            nal = self._dat.find(self._sep, nal + len(self._sep))
 
     def flush(self):
         pass
@@ -60,16 +64,28 @@ class RPiH264Stream:
 
 
 if __name__ == '__main__':
-    stream = RPiH264Stream(RPiCameraConfig(
+    # stream = RPiStream(RPiCameraConfig(
+    #     width=1920,
+    #     height=1080,
+    #     fps=30,
+    #     quality=20))
+
+    # with open('tmp.h264', 'wb') as f:
+    #     def on_frame(frame):
+    #         f.write(frame)
+    #     stream.on_frame(on_frame)
+    #     stream.start(number_secs=10)
+
+    stream = RPiStream(RPiCameraConfig(
         width=1920,
         height=1080,
-        fps=30,
+        fps=2,
+        bps=25000000,
+        format='mjpeg',
         quality=20))
 
-    with open('tmp.h264', 'wb') as f:
-        def on_frame(frame):
-            f.write(frame)
-        stream.on_frame(on_frame)
-        stream.start(number_secs=10)
-
+    def on_frame(frame):
+        print(len(frame))
+    stream.on_frame(on_frame)
+    stream.start(number_secs=10)
 
